044e266453fe1f73f7cf38b03efff18e7aeebe6a
GalchukRuth <ruth77606.gmail.com>
2018-07-04 22:03:47 +0300

    Check_Monitin.py, Client.py, Monitin_Dir added

---
+++ Check_Monitin.py
@@ -0,0 +1,95 @@
+import hashlib
+import os
+import requests
+import time
+
+URL = 'https://www.virustotal.com/vtapi/v2/file/report'
+API_KEY = '471a3e27495d9d7af52c760de32ff44242d027a55ce55990ce910db00fcec3c6'
+
+# Getting file content
+def fileContent(file_path):
+    with open(file_path, 'rb')as f:
+        content = f.read()
+    return content
+
+# Calculate file hash
+def file2Hash(content):
+    h = hashlib.md5()
+    h.update(content)
+    return h.hexdigest()
+
+# Checking file for executable extension (exe, dll, sys) or content (MZ)
+def isExecutable(content):
+    str_mz = 'MZ'
+    str_exe = '.exe'
+    str_sys = '.sys'
+    str_SYS = '.SYS'
+    str_dll = '.dll'
+    if content.find(str_mz) == 0:
+        if content.find(str_exe) > -1:
+            status = 'mz exe'
+        elif content.find(str_dll) > -1:
+            status = 'mz dll'
+        elif content.find(str_sys) > -1:
+            status = 'mz sys'
+        elif content.find(str_SYS) > -1:
+            status = 'mz SYS'
+        else:
+            status = 'mz without exe'
+    else:
+        status = 'no mz'
+    return status
+
+# Getting change time for file
+def getTime(file_path):
+    file_time = os.path.getctime(file_path)
+    return time.strftime("%d %b %Y %H:%M:%S", time.gmtime(file_time))
+
+# Calculating rate by hash from VirusTotal site
+def getRateFromVirusTotal(hash):
+    response = requests.get(URL, params={'apikey':API_KEY,'resource':hash})
+    data = response.json()
+    if data['verbose_msg'] == 'Scan finished, information embedded':
+        pos = data['positives']
+        total = data['total']
+        rate = float(pos) / float(total)
+    else:
+        rate = 0.9
+    return rate
+
+# Calculating monitin from single file -> to dictionary ---> not used
+def singleFile2Dic(file_path):
+    dic = {}
+    f_size = float(os.path.getsize (file_path)) / 1024
+    content = fileContent(file_path)
+    f_hash = file2Hash(content)
+    f_exec = isExecutable(content)
+    f_time = getTime(file_path)
+    f_rate = getRateFromVirusTotal(f_hash)
+    dic.update({'md5': f_hash, 'is_exec': f_exec, 'time': f_time, 'rate': f_rate, 'size': f_size})
+    return dic
+
+# Calculating monitin from single file -> to list
+def singleFile2List(file_path):
+    lst = []
+    content = fileContent(file_path)
+    f_hash = file2Hash(content)
+    lst.append(float(os.path.getsize (file_path)) / 1024)
+    lst.append(isExecutable(content))
+    lst.append(getTime(file_path))
+    lst.append(getRateFromVirusTotal(f_hash))
+    return lst
+
+# Calculating monitin for all files under a folder
+def directory(dir_path):
+    dic = {}
+    for i in os.listdir(dir_path):
+        full_path = dir_path + '\\' + i
+        if os.path.isfile(full_path):
+            dic[i] = singleFile2List(full_path)
+    return sortDicByRate(dic)
+
+# Sorting by rate
+def sortDicByRate(dic):
+    sort_lst = sorted(dic.iteritems(), key=lambda v: v[1][3], reverse=True)
+    return sort_lst
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Monitin
-Software product that checking monitin of files
+SIEM project by Ruth Galchuk
+============================
+Software product that checking monitin of files.
+It can read files and folders, and give the following information on each one:
+a.	MD5 hash
+b.	Is it an executable?
+c.	Date changed (readable time)
+d.  File size in Mb
+e.  Rate
+
+Files
+-----
+- Check_Monitin.py -
+- Client.py -
+- Monitin_Dir - directory with 4 files from checking
+
+Instructions
+------------
+2.	Save all the data in a dictionary called "Results".
+3.	Add a "rate" to each scanned file.
+4.	Go over the example json response. What does the field "verbose message" do? Is it useful in our project?
+5.	Under each detecting software, there are a few sub-fields. What is the field "result"?
+6.	What is the field "update" and why is it relevant?
+7.	After you've examined the response structuere, your Monitin project should check files upon the VirusTotal database.
+8.	Check your connectivity, and try to send a test hash (get a hash of something known, like cmd.exe). Send the hash and print out the whole json you're getting in return.
+9.	After testing successfully, add the API requests as part of your code. For every file, in addition to the data that you've printed before (question 1 above), add a "rate" field, based on the VirusTotal response.
+The flow is:
+a.	Send the hash to the API and save the result.
+b.	Translate it to json and read the relevant fields.
+c.	Was the hash found in the database? If not, give it a rate of 0.9 (dangerous).
+d.	If the hash was found, calculate its danger rate and add it to the Results dictionary.
+e.	Print the amount of viruses that have identified the file as malware.
+
+* Make sure you test using single files, so you don't exceed the free limits.
+
+
